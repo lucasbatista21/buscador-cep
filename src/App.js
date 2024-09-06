a9f9ec75bd0a4abdd551562345a1d84e2b6d1d95
lukitahai7 <lucasbo2007@gmail.com>
2024-09-06 10:25:11 -0300

    Importação de biblioteca e ajustes
    Importei a biblioteca react-icons e prossegui no HTML do site

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { FiSearch } from "react-icons/fi";
 function App() {
   return <div className="container">
     <h1 className="title">Buscador de CEP</h1>
@@ -7,6 +8,17 @@ function App() {
         type="text"
         placeholder="Digite seu CEP..."
         />
+
+        <button className="btnSearch">
+          <FiSearch size={25} color="#fff"/>
+        </button>
+
+        <main className="main-container">
+            <h2>CEP: 07912572</h2>
+
+            <span>Rua Teste algum</span>
+            <span>Complemento: Algum complemento</span>
+        </main>
     </div>
   </div>;
 }
