0911c934a826fdce69af074ace445ad886f0bf58
lukitahai7 <lucasbo2007@gmail.com>
2024-09-06 10:51:27 -0300

    Início do CSS e animação
    Criei e iniciei o arquivo styles.css e coloquei uma animação no título "Buscado de CEP"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { FiSearch } from "react-icons/fi";
+import "./styles.css";
+
 function App() {
   return <div className="container">
     <h1 className="title">Buscador de CEP</h1>
@@ -18,6 +20,8 @@ function App() {
 
             <span>Rua Teste algum</span>
             <span>Complemento: Algum complemento</span>
+            <span>Vila Rosa</span>
+            <span>Mauá, SP</span>
         </main>
     </div>
   </div>;
